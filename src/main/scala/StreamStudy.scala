e5b54bf275840feeffdf6e5b88dab709d6eb80c8
hosh294 <132256088+hosh294@users.noreply.github.com>
2023-05-15 12:24:31 +0900

    lazy valを用いて値が保持されるよう変更

--- src/main/scala/StreamStudy.scala
+++ src/main/scala/StreamStudy.scala
@@ -1,19 +1,28 @@
-trait StreamStudy[+A] {
+trait LazyListStudy[+A] {
 
-  def headOption: Option[A] = ???
+  def headOption: Option[A] = this match {
+    case EmptyLazyList => None
+    case Cons(h, _) => Some(h())
+  }
 
-  def tail: StreamStudy[A] = ???
-
+  def tail: LazyListStudy[A] = this match {
+    case EmptyLazyList => throw new Error()
+    case Cons(_, t) => t()
+  }
 }
 
-case object EmptyStream extends StreamStudy[Nothing]
+case object EmptyLazyList extends LazyListStudy[Nothing]
 
-case class Cons[+A](h: () => A, t: () => StreamStudy[A]) extends StreamStudy[A]
+case class Cons[+A](h: () => A, t: () => LazyListStudy[A]) extends LazyListStudy[A]
 
-object StreamStudy {
+object LazyListStudy {
 
-  def cons[A](h: => A, t: => StreamStudy[A]): StreamStudy[A] = ???
+  def cons[A](h: => A, t: => LazyListStudy[A]): LazyListStudy[A] = {
+    lazy val head = h
+    lazy val tail = t
+    Cons(() => head, () => tail)
+  }
 
-  def empty[A]: StreamStudy[A] = EmptyStream
+  def empty[A]: LazyListStudy[A] = EmptyLazyList
 
 }
